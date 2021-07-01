93e167ef741df1301687ee8f970da2f058c93ec2
madoca.kirameki <madoca.kirameki@techacademy.jp>
2021-07-01 12:53:50 +0900

    typescript 課題2　指摘対応

--- typescript/kadai2/dist/index.js
+++ typescript/kadai2/dist/index.js
@@ -1,29 +1,27 @@
 "use strict";
+const R = require('ramda');
 class ObjectWrapper {
     /***
      * 引数のオブジェクトのコピーを this._objに設定
      */
     constructor(_obj) {
-        this._obj = Object.assign({}, _obj);
+        this._obj = R.clone(_obj);
     }
     /**
      * this._objのコピーを返却
      * @return Object
      */
-    get obj() { return Object.assign({}, this._obj); }
+    get obj() {
+        return R.clone(this._obj);
+    }
     /**
      * this._obj[key] に valを設定。keyがthis._objに存在しない場合、falseを返却
      * @param key オブジェクトのキー
      * @param val オブジェクトの値
      */
     set(key, val) {
-        if (key in this._obj) {
-            this._obj[key] = val;
-            return true;
-        }
-        else {
-            return false;
-        }
+        this._obj[key] = val;
+        return true;
     }
     /**
      * 指定したキーの値のコピーを返却
@@ -31,13 +29,8 @@ class ObjectWrapper {
      * @param key オブジェクトのキー
      */
     get(key) {
-        if (key in this._obj) {
-            const val = this._obj[key];
-            return val;
-        }
-        else {
-            return undefined;
-        }
+        const val = this._obj[key];
+        return val;
     }
     /**
      * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
@@ -65,15 +58,17 @@ if (wrappedObj1.obj.a === '01') {
 else {
     console.error('NG: get obj()');
 }
-if (wrappedObj1.set('c', '03') === false &&
-    wrappedObj1.set('b', '04') === true &&
+if (
+// wrappedObj1.set('c', '03') === false &&
+wrappedObj1.set('b', '04') === true &&
     wrappedObj1.obj.b === '04') {
     console.log('OK: set(key, val)');
 }
 else {
     console.error('NG: set(key, val)');
 }
-if (wrappedObj1.get('b') === '04' && wrappedObj1.get('c') === undefined) {
+if (wrappedObj1.get('b') === '04' // && wrappedObj1.get('c') === undefined
+) {
     console.log('OK: get(key)');
 }
 else {
--- typescript/kadai2/src/index.ts
+++ typescript/kadai2/src/index.ts
@@ -1,33 +1,31 @@
-type objType =  { [key: string]: string };
+const R = require('ramda');
 
-class ObjectWrapper {
-  private _obj:objType;
+class ObjectWrapper<T> {
+  private _obj: T;
 
   /***
    * 引数のオブジェクトのコピーを this._objに設定
    */
-  constructor( _obj: objType) {
-      this._obj = {..._obj};
+  constructor(_obj: T) {
+    this._obj = R.clone(_obj);
   }
 
   /**
    * this._objのコピーを返却
    * @return Object
    */
-  get obj(): objType { return {...this._obj}; }
+  get obj(): T {
+    return R.clone(this._obj);
+  }
 
   /**
    * this._obj[key] に valを設定。keyがthis._objに存在しない場合、falseを返却
    * @param key オブジェクトのキー
    * @param val オブジェクトの値
    */
-  set(key: string, val: string): boolean {
-      if(key in this._obj){
-          this._obj[key] = val;
-          return true;
-      }else{
-          return false;
-      }
+  set<K extends keyof T, U extends T[K]>(key: K, val: U): boolean {
+    this._obj[key] = val;
+    return true;
   }
 
   /**
@@ -35,27 +33,23 @@ class ObjectWrapper {
    * 指定のキーが存在しない場合 undefinedを返却
    * @param key オブジェクトのキー
    */
-  get(key: string) : string | undefined {
-      if(key in this._obj){
-          const val: string = this._obj[key];
-          return val;
-      }else{
-          return undefined;
-      }
+  get<K extends keyof T>(key: K): T[K] {
+    const val: T[K] = this._obj[key];
+    return val;
   }
 
   /**
    * 指定した値を持つkeyの配列を返却。該当のものがなければ空の配列を返却。
    */
-  findKeys(val: string) : string[] {
-      let targetKeyArray: string[] = [];
-      const allKeyArray = Object.keys(this._obj);
-      allKeyArray.forEach(element => {
-          if(this._obj[element] === val){
-              targetKeyArray.push(element);
-          }
-      });
-      return targetKeyArray;
+  findKeys<K extends keyof T, U extends T[K]>(val: U): K[] {
+    let targetKeyArray: K[] = [];
+    const allKeyArray = Object.keys(this._obj) as K[];
+    allKeyArray.forEach(element => {
+      if (this._obj[element] === val) {
+        targetKeyArray.push(element);
+      }
+    });
+    return targetKeyArray;
   }
 }
 
@@ -64,7 +58,7 @@ class ObjectWrapper {
  * 完成したら、以下のスクリプトがすべてOKになる。
  */
 const obj1 = { a: '01', b: '02' };
-const wrappedObj1: ObjectWrapper = new ObjectWrapper(obj1);
+const wrappedObj1 = new ObjectWrapper(obj1);
 
 if (wrappedObj1.obj.a === '01') {
   console.log('OK: get obj()');
@@ -73,7 +67,7 @@ if (wrappedObj1.obj.a === '01') {
 }
 
 if (
-  wrappedObj1.set('c', '03') === false &&
+  // wrappedObj1.set('c', '03') === false &&
   wrappedObj1.set('b', '04') === true &&
   wrappedObj1.obj.b === '04'
 ) {
@@ -82,7 +76,10 @@ if (
   console.error('NG: set(key, val)');
 }
 
-if (wrappedObj1.get('b') === '04' && wrappedObj1.get('c') === undefined) {
+if (
+  wrappedObj1.get('b') === '04'
+  // && wrappedObj1.get('c') === undefined
+) {
   console.log('OK: get(key)');
 } else {
   console.error('NG: get(key)');
